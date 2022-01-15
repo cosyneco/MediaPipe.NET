4b3bb84d685ce2d8ddf5e446c0af4b5aa49d710b
Speykious <speykious@gmail.com>
2022-01-15 03:34:28 +0100

    Port safe part of `CalculatorGraph`

---
+++ Mediapipe.Net/Native/SafeNativeMethods/Framework/CalculatorGraph.cs
@@ -0,0 +1,32 @@
+// Copyright (c) homuler and Vignette
+// This file is part of MediaPipe.NET.
+// MediaPipe.NET is licensed under the MIT License. See LICENSE for details.
+
+using System;
+using System.Diagnostics.Contracts;
+using System.Runtime.InteropServices;
+
+namespace Mediapipe.Net.Native;
+
+internal partial class SafeNativeMethods : NativeMethods
+{
+    [Pure, DllImport(MEDIAPIPE_LIBRARY, ExactSpelling = true)]
+    [return: MarshalAs(UnmanagedType.I1)]
+    public static extern bool mp_CalculatorGraph__HasError(IntPtr graph);
+
+    [Pure, DllImport(MEDIAPIPE_LIBRARY, ExactSpelling = true, CharSet = CharSet.Unicode)]
+    [return: MarshalAs(UnmanagedType.I1)]
+    public static extern bool mp_CalculatorGraph__HasInputStream__PKc(IntPtr graph, string name);
+
+    [Pure, DllImport(MEDIAPIPE_LIBRARY, ExactSpelling = true)]
+    [return: MarshalAs(UnmanagedType.I1)]
+    public static extern bool mp_CalculatorGraph__GraphInputStreamsClosed(IntPtr graph);
+
+    [Pure, DllImport(MEDIAPIPE_LIBRARY, ExactSpelling = true)]
+    [return: MarshalAs(UnmanagedType.I1)]
+    public static extern bool mp_CalculatorGraph__IsNodeThrottled__i(IntPtr graph, int nodeId);
+
+    [Pure, DllImport(MEDIAPIPE_LIBRARY, ExactSpelling = true)]
+    [return: MarshalAs(UnmanagedType.I1)]
+    public static extern bool mp_CalculatorGraph__UnthrottleSources(IntPtr graph);
+}
