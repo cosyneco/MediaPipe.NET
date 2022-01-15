feb19f3127b332031c8a7ca8a018d9cbdef031a2
Speykious <speykious@gmail.com>
2022-01-15 07:21:14 +0100

    Port `Protobuf`

---
+++ Mediapipe.Net/Native/UnsafeNativeMethods/External/Protobuf.cs
@@ -0,0 +1,70 @@
+// Copyright (c) homuler and Vignette
+// This file is part of MediaPipe.NET.
+// MediaPipe.NET is licensed under the MIT License. See LICENSE for details.
+
+using System;
+using System.Runtime.InteropServices;
+using Mediapipe.Net.External;
+
+namespace Mediapipe.Net.Native
+{
+    internal partial class UnsafeNativeMethods : NativeMethods
+    {
+        [DllImport(MEDIAPIPE_LIBRARY, ExactSpelling = true)]
+        public static extern MpReturnCode google_protobuf__SetLogHandler__PF(
+            [MarshalAs(UnmanagedType.FunctionPtr)] Protobuf.ProtobufLogHandler logHandler);
+
+        [DllImport(MEDIAPIPE_LIBRARY, ExactSpelling = true)]
+        public static extern void mp_api_SerializedProtoArray__delete(IntPtr serializedProtoVectorData);
+
+        #region MessageProto
+        [DllImport(MEDIAPIPE_LIBRARY, ExactSpelling = true)]
+        public static extern MpReturnCode mp_Packet__GetClassificationList(IntPtr packet, out SerializedProto serializedProto);
+
+        [DllImport(MEDIAPIPE_LIBRARY, ExactSpelling = true)]
+        public static extern MpReturnCode mp_Packet__GetClassificationListVector(IntPtr packet, out SerializedProtoVector serializedProtoVector);
+
+        [DllImport(MEDIAPIPE_LIBRARY, ExactSpelling = true)]
+        public static extern MpReturnCode mp_Packet__GetDetection(IntPtr packet, out SerializedProto serializedProto);
+
+        [DllImport(MEDIAPIPE_LIBRARY, ExactSpelling = true)]
+        public static extern MpReturnCode mp_Packet__GetDetectionVector(IntPtr packet, out SerializedProtoVector serializedProtoVector);
+
+        [DllImport(MEDIAPIPE_LIBRARY, ExactSpelling = true)]
+        public static extern MpReturnCode mp_Packet__GetFaceGeometry(IntPtr packet, out SerializedProto serializedProto);
+
+        [DllImport(MEDIAPIPE_LIBRARY, ExactSpelling = true)]
+        public static extern MpReturnCode mp_Packet__GetFaceGeometryVector(IntPtr packet, out SerializedProtoVector serializedProtoVector);
+
+        [DllImport(MEDIAPIPE_LIBRARY, ExactSpelling = true)]
+        public static extern MpReturnCode mp_Packet__GetFrameAnnotation(IntPtr packet, out SerializedProto serializedProto);
+
+        [DllImport(MEDIAPIPE_LIBRARY, ExactSpelling = true)]
+        public static extern MpReturnCode mp_Packet__GetLandmarkList(IntPtr packet, out SerializedProto serializedProto);
+
+        [DllImport(MEDIAPIPE_LIBRARY, ExactSpelling = true)]
+        public static extern MpReturnCode mp_Packet__GetLandmarkListVector(IntPtr packet, out SerializedProtoVector serializedProtoVector);
+
+        [DllImport(MEDIAPIPE_LIBRARY, ExactSpelling = true)]
+        public static extern MpReturnCode mp_Packet__GetNormalizedLandmarkList(IntPtr packet, out SerializedProto serializedProto);
+
+        [DllImport(MEDIAPIPE_LIBRARY, ExactSpelling = true)]
+        public static extern MpReturnCode mp_Packet__GetNormalizedLandmarkListVector(IntPtr packet, out SerializedProtoVector serializedProtoVector);
+
+        [DllImport(MEDIAPIPE_LIBRARY, ExactSpelling = true)]
+        public static extern MpReturnCode mp_Packet__GetNormalizedRect(IntPtr packet, out SerializedProto serializedProto);
+
+        [DllImport(MEDIAPIPE_LIBRARY, ExactSpelling = true)]
+        public static extern MpReturnCode mp_Packet__GetNormalizedRectVector(IntPtr packet, out SerializedProtoVector serializedProtoVector);
+
+        [DllImport(MEDIAPIPE_LIBRARY, ExactSpelling = true)]
+        public static extern MpReturnCode mp_Packet__GetRect(IntPtr packet, out SerializedProto serializedProto);
+
+        [DllImport(MEDIAPIPE_LIBRARY, ExactSpelling = true)]
+        public static extern MpReturnCode mp_Packet__GetRectVector(IntPtr packet, out SerializedProtoVector serializedProtoVector);
+
+        [DllImport(MEDIAPIPE_LIBRARY, ExactSpelling = true)]
+        public static extern MpReturnCode mp_Packet__GetTimedModelMatrixProtoList(IntPtr packet, out SerializedProto serializedProto);
+        #endregion
+    }
+}
