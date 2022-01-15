38844436c96b07c6cd02aa2ae4a086042f35537c
Speykious <speykious@gmail.com>
2022-01-15 04:40:17 +0100

    Port unsafe part of `CalculatorGraph`

---
+++ Mediapipe.Net/Native/UnsafeNativeMethods/Framework/CalculatorGraph.cs
@@ -0,0 +1,15 @@
+// Copyright (c) homuler and Vignette
+// This file is part of MediaPipe.NET.
+// MediaPipe.NET is licensed under the MIT License. See LICENSE for details.
+
+using System.Runtime.InteropServices;
+using Mediapipe.Net.External;
+
+namespace Mediapipe.Net.Native;
+
+internal partial class UnsafeNativeMethods : NativeMethods
+{
+    [DllImport(MEDIAPIPE_LIBRARY, ExactSpelling = true, CharSet = CharSet.Unicode)]
+    [return: MarshalAs(UnmanagedType.I1)]
+    public static extern bool mp_api__ConvertFromCalculatorGraphConfigTextFormat(string configText, out SerializedProto serializedProto);
+}
