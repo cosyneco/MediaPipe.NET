f074059bd363f08a871212672eef9229dbbe7fad
Speykious <speykious@gmail.com>
2022-01-15 07:27:03 +0100

    Port `InstantMotionTracking`

---
+++ Mediapipe.Net/Graphs/InstantMotionTracking/Anchor3dVector.cs
@@ -0,0 +1,35 @@
+// Copyright (c) homuler and Vignette
+// This file is part of MediaPipe.NET.
+// MediaPipe.NET is licensed under the MIT License. See LICENSE for details.
+
+using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using Mediapipe.Net.Native;
+
+namespace Mediapipe.Net.Graphs.InstantMotionTracking
+{
+    [StructLayout(LayoutKind.Sequential)]
+    internal struct Anchor3dVector
+    {
+        public IntPtr Data;
+        public int Size;
+
+        public void Dispose() => UnsafeNativeMethods.mp_Anchor3dArray__delete(Data);
+
+        public List<Anchor3d> ToList()
+        {
+            var anchors = new List<Anchor3d>(Size);
+
+            unsafe
+            {
+                var anchorPtr = (Anchor3d*)Data;
+
+                for (var i = 0; i < Size; i++)
+                    anchors.Add(Marshal.PtrToStructure<Anchor3d>((IntPtr)anchorPtr++));
+            }
+
+            return anchors;
+        }
+    }
+}
