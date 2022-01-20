7f1b2ffb6b14c6f26317dbb64bdcfade30a4a1d3
Speykious <speykious@gmail.com>
2022-01-20 11:19:33 +0100

    Port test attributes

---
+++ Mediapipe.Net.Tests/GpuOnlyAttribute.cs
@@ -0,0 +1,12 @@
+// Copyright (c) homuler and Vignette
+// This file is part of MediaPipe.NET.
+// MediaPipe.NET is licensed under the MIT License. See LICENSE for details.
+
+using System;
+using NUnit.Framework;
+
+namespace Mediapipe.Net.Tests
+{
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
+    public class GpuOnlyAttribute : CategoryAttribute { }
+}
