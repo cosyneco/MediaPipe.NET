eace687473745f772081d773bcad59698b8e4463
Ayane <ayane@vignetteapp.org>
2023-02-15 13:38:20 +0800

    Make SerializedProto actually IDisposable
    Signed-off-by: Ayane <ayane@vignetteapp.org>

--- Mediapipe.Net/Native/SafeNativeMethods/External/Absl.cs
+++ Mediapipe.Net/Native/SafeNativeMethods/External/Absl.cs
@@ -2,6 +2,7 @@
 // This file is part of MediaPipe.NET.
 // MediaPipe.NET is licensed under the MIT License. See LICENSE for details.
 
+using System;
 using System.Diagnostics.Contracts;
 using System.Runtime.InteropServices;
 
@@ -10,9 +11,9 @@ namespace Mediapipe.Net.Native
     internal unsafe partial class SafeNativeMethods : NativeMethods
     {
         [Pure, DllImport(MEDIAPIPE_LIBRARY, ExactSpelling = true)]
-        public static extern byte absl_Status__ok(void* status);
+        public static extern byte absl_Status__ok(IntPtr status);
 
         [Pure, DllImport(MEDIAPIPE_LIBRARY, ExactSpelling = true)]
-        public static extern int absl_Status__raw_code(void* status);
+        public static extern int absl_Status__raw_code(IntPtr status);
     }
 }
--- Mediapipe.Net/Native/UnsafeNativeMethods/External/Absl.cs
+++ Mediapipe.Net/Native/UnsafeNativeMethods/External/Absl.cs
@@ -2,6 +2,7 @@
 // This file is part of MediaPipe.NET.
 // MediaPipe.NET is licensed under the MIT License. See LICENSE for details.
 
+using System;
 using System.Runtime.InteropServices;
 
 namespace Mediapipe.Net.Native
@@ -10,13 +11,13 @@ namespace Mediapipe.Net.Native
     {
 #pragma warning disable CA2101
         [DllImport(MEDIAPIPE_LIBRARY, ExactSpelling = true, CharSet = CharSet.Ansi)]
-        public static extern MpReturnCode absl_Status__i_PKc(int code, string message, out void* status);
+        public static extern MpReturnCode absl_Status__i_PKc(int code, string message, out IntPtr status);
 
         [DllImport(MEDIAPIPE_LIBRARY, ExactSpelling = true)]
-        public static extern void absl_Status__delete(void* status);
+        public static extern void absl_Status__delete(IntPtr status);
 
         [DllImport(MEDIAPIPE_LIBRARY, ExactSpelling = true)]
-        public static extern MpReturnCode absl_Status__ToString(void* status, out sbyte* str);
+        public static extern MpReturnCode absl_Status__ToString(IntPtr status, out IntPtr str);
 #pragma warning restore CA2101
     }
 }
