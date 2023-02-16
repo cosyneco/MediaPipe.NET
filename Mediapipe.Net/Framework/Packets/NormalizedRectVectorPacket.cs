9dda4a6ff238f838b0578829899ec3989eeca456
Ayane <ayane@vignetteapp.org>
2023-02-16 15:18:51 +0800

    Finsih RectVector
    Signed-off-by: Ayane <ayane@vignetteapp.org>

--- Mediapipe.Net/Framework/Packets/NormalizedRectVectorPacket.cs
+++ Mediapipe.Net/Framework/Packets/NormalizedRectVectorPacket.cs
@@ -7,9 +7,6 @@ using Mediapipe.Net.Framework.Protobuf;
 using Mediapipe.Net.Native;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Mediapipe.Net.Framework.Packets
 {
---
+++ Mediapipe.Net/Framework/Packets/RectVectorPacket.cs
@@ -0,0 +1,42 @@
+// Copyright (c) homuler and The Vignette Authors
+// This file is part of MediaPipe.NET.
+// MediaPipe.NET is licensed under the MIT License. See LICENSE for details.
+
+using Mediapipe.Net.Framework.Port;
+using Mediapipe.Net.Framework.Protobuf;
+using Mediapipe.Net.Native;
+using System;
+using System.Collections.Generic;
+
+namespace Mediapipe.Net.Framework.Packets
+{
+    internal class RectVectorPacket : Packet<List<Rect>>
+    {
+        public RectVectorPacket() : base(true) { }
+        public RectVectorPacket(IntPtr ptr, bool isOwner = true) : base(ptr, isOwner) { }
+        public RectVectorPacket? At(Timestamp timestamp) => At<RectVectorPacket>(timestamp);
+
+        public override List<Rect> Get()
+        {
+            UnsafeNativeMethods.mp_Packet__GetRectVector(MpPtr, out var serializedProtoVector).Assert();
+            GC.KeepAlive(this);
+
+            var rect = serializedProtoVector.Deserialize(Rect.Parser);
+            serializedProtoVector.Dispose();
+
+            return rect;
+        }
+
+        public override StatusOr<List<Rect>> Consume()
+        {
+            throw new NotSupportedException();
+        }
+
+        public override Status ValidateAsType()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    {
+    }
+}
