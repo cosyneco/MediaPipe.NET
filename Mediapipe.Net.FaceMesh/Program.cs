215a614d4dbb730a5dcb7a27c945fa512e062c3a
Robin Bigeard <robin.bigeard@public-i.info>
2024-05-28 15:05:45 +0100

    added landmark

--- Mediapipe.Net.FaceMesh/Program.cs
+++ Mediapipe.Net.FaceMesh/Program.cs
@@ -5,6 +5,7 @@ using OpenCvSharp;
 using System.IO;
 using Mediapipe.Framework.Formats;
 using Mediapipe;
+using System.Collections.Generic;
 
 namespace MediapipeNet2
 {
@@ -25,6 +26,7 @@ namespace MediapipeNet2
         private readonly Window window;
         private readonly CalculatorGraph graph;
         private readonly OutputStreamPoller<ImageFrame> poller;
+        private readonly OutputStreamPoller<List<NormalizedLandmarkList>> landmarkPoller;
         private readonly Mat captureMat;
         private readonly Mat outputMat;
 
@@ -37,6 +39,7 @@ namespace MediapipeNet2
 
             graph = new CalculatorGraph(File.ReadAllText(@"face_mesh_desktop_live.pbtxt"));
             poller = graph.AddOutputStreamPoller<ImageFrame>("output_video");
+            landmarkPoller = graph.AddOutputStreamPoller<List<NormalizedLandmarkList>>("multi_face_landmarks");
             graph.StartRun();
 
             captureMat = new Mat();
@@ -82,7 +85,7 @@ namespace MediapipeNet2
                             new long[] { outputFrame.WidthStep(), outputFrame.ByteDepth() }
                         ))
                         {
-                            window.ShowImage(outputMat);
+                            window.ShowImage(MouthMovement(captureMat));
                         }
                     }
                 }
@@ -97,6 +100,51 @@ namespace MediapipeNet2
         {
             byte* ptr = (byte*)mat.Data.ToPointer();
             return new ReadOnlySpan<byte>(ptr, mat.Width * mat.Height * mat.Channels());
+        }
+        
+        private Mat MouthMovement(Mat outputMat)
+        {
+            var pOutputLandmark = new Packet<List<NormalizedLandmarkList>>();
+            var pOutputRectLandmark = new Packet<List<NormalizedRect>>();
+
+            if (!landmarkPoller.Next(pOutputLandmark))
+            {
+                return outputMat;
+            }
+
+            var outputLandmark = pOutputLandmark.Get(NormalizedLandmarkList.Parser);
+
+            var i = 1;
+            outputLandmark.ForEach((l) =>
+            {
+                var upperLip = l.Landmark[13];
+                var lowerLip = l.Landmark[14];
+                var lipDistancePixels = Math.Abs(upperLip.Y - lowerLip.Y);
+
+                var leftEyeCorner = l.Landmark[159];
+                var rightEyeCorner = l.Landmark[386];
+                var distancePixels = Math.Sqrt(Math.Pow(leftEyeCorner.X - rightEyeCorner.X, 2) + Math.Pow(leftEyeCorner.Y - rightEyeCorner.Y, 2));
+
+                var initialThreshold = 0.01;
+                var averageFaceSizeCm = 20.0;
+                var averageFaceSizePixels = 300.0;
+
+                var distanceCm = (averageFaceSizeCm * averageFaceSizePixels) / distancePixels;
+                var threshold = (130 - (distanceCm / 5000)) * (0.001 / (distanceCm / 5000));
+
+                if (lipDistancePixels > threshold)
+                {
+                    outputMat.PutText($"- OPEN ({i})", new Point(upperLip.X * outputMat.Width, upperLip.Y * outputMat.Height - 60), HersheyFonts.HersheySimplex, 0.7, Scalar.Violet, 2);
+                }
+                else
+                {
+                    outputMat.PutText($"- CLOSE ({i})", new Point(upperLip.X * outputMat.Width, upperLip.Y * outputMat.Height - 60), HersheyFonts.HersheySimplex, 0.7, Scalar.Blue, 2);
+                }
+                i++;
+                captureMat.Circle(new Point(upperLip.X * outputMat.Width, upperLip.Y * outputMat.Height), 1, Scalar.Red, 1);
+                captureMat.Circle(new Point(lowerLip.X * outputMat.Width, lowerLip.Y * outputMat.Height), 1, Scalar.Green, 1);
+            });
+            return outputMat;
         }
 
         public void Dispose()
